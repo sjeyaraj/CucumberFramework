e2e6109cca53104542bd54f6a6dcdd942edb3c0e
stephen <j_h_stephen@yahoo.com>
2019-10-14 15:59:45 -0500

    first commit

---
+++ src/main/java/com/cucumberfw/CucumberFramework/App.java
@@ -0,0 +1,13 @@
+package com.cucumberfw.CucumberFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
